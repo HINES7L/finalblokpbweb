aa50ca17ed8f2d5de8ebf58c475d0aaee392f603
unknown <you@example.com>
2024-12-08 20:03:05 +0700

    first commit

---
+++ app/Views/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+echo session()->get('u');
+?>
+<main id="main" class="main">
+
+	<div class="pagetitle">
+		<h1>PENGERTIAN</h1>
+		<nav>
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+				<li class="breadcrumb-item">Tables</li>
+				<li class="breadcrumb-item active">Conclusion</li>
+			</ol>
+		</nav>
+	</div><!-- End Page Title -->
+
+
+	<section class="section">
+		<div class="row align-items-top">
+			<div class="col-lg-12">
+
+				<!-- Default Card -->
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">PERPUASTAKAAN</h5>
+						Perpustakaan sekolah online, atau yang lebih dikenal sebagai perpustakaan digital, merupakan sistem perpustakaan modern yang dirancang untuk menyimpan, mengelola, dan mendistribusikan berbagai jenis informasi dalam format elektronik. Informasi tersebut dapat berupa buku digital (e-book), gambar, rekaman audio, video pembelajaran, jurnal elektronik, hingga dokumen-dokumen akademik lainnya. Berbeda dengan perpustakaan konvensional yang mengandalkan koleksi fisik seperti buku cetak dan majalah, perpustakaan digital menawarkan kemudahan akses tanpa batasan geografis atau waktu.
+
+						Dengan memanfaatkan jaringan komputer dan teknologi internet, perpustakaan digital memungkinkan pengguna untuk mengakses berbagai sumber daya informasi kapan saja dan dari mana saja. Selama terhubung dengan jaringan, baik melalui komputer, laptop, maupun perangkat seluler seperti ponsel pintar atau tablet, pengguna dapat menjelajahi dan menggunakan koleksi perpustakaan tanpa harus datang langsung ke lokasi fisik.
+					</div>
+				</div><!-- End Default Card -->
---
+++ app/Views/tablepeminjaman.php
@@ -0,0 +1,43 @@
+<table>
+  <tr>
+   <td width="10%"><img src="<?= base_url('img/logoobor.png');?>" width="80%"></td>
+   <td width="250%">Perpustakaan</td>
+ </tr>
+</table>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th width="3%">NO</th>
+      <th>Id Siswa</th>
+      <th>Id Buku</th>
+      <th>Id Petugas</th>
+      <th>Tanggal Peminjaman</th>
+      <th>Tanggal Pengembalian</th>
+      <th>Denda</th>
+      <th>jumlah</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    <?php
+    $ms=1;
+    foreach ($takdirestui as $key => $value) {
+      ?>
+      <tr>
+        <td><?= $ms++ ?></td>
+        <td><?= $value->id_siswa ?></td>
+        <td><?= $value->id_buku ?></td>
+        <td><?= $value->id_petugas ?></td>
+        <td><?= $value->tanggal_peminjaman ?></td>
+        <td><?= $value->tanggal_pengembalian ?></td>
+        <td><?= $value->denda ?></td>
+        <td><?= $value->jumlah ?></td>
+      </tr>
+      <?php 
+    } 
+    ?>
+  </tbody>
+</table>
+<script>
+  window.print();
+</script>
